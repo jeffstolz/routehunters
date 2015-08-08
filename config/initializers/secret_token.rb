cb474e2fa20900c16bd16bac2cc1d8133279a643
Jeff Stolz <jeffstolzdesign@gmail.com>
2015-08-08 12:11:46 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Routehunters::Application.config.secret_key_base = '1c52825aaf9510cdd9aac1dcc3bbb1bed1fb535e65b75382f476888fe711fd44d119df77785691b036e6cb925b1ab5e9dfa7f3e733b6974328d6170bb046798d'
